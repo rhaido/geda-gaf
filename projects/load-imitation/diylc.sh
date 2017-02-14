2ff3b79a2ac682d11117cc80a1769f8300cc05e6
Mike Grozak <mike [d0t] grozak [at] gmail [d0t] com>
2017-02-14 17:46:03 +0100

    added stripboard plan for diylc

---
+++ projects/load-imitation/diylc.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+export _JAVA_AWT_WM_NONREPARENTING=1
+export _JAVA_OPTIONS="-Dawt.useSystemAAFontSettings=on -Dswing.aatext=true -Dswing.defaultlaf=com.sun.java.swing.plaf.gtk.GTKLookAndFeel -Dsun.java2d.opengl=true"
